910e6cc9e92a2358d38db02f02698c02e2570591
Daniel Toader <daniel.t.pdx@gmail.com>
2015-03-06 09:38:15 -0800

    complete contact class with encapsulation, getters, and setters

--- src/contact.php
+++ src/contact.php
@@ -1,23 +1,54 @@
 <?php
-    class Contact
-    {
+    class Contact {
         private $name;
+        private $phone;
+        private $address;
 
-        function __construct()
-        {
-            $this->
+        function __construct($name, $phone, $address) {
+            $this->name = $name;
+            $this->phone = $phone;
+            $this->address = $address;
         }
 
         // getters
+        function getName() {
+            return $this->name;
+        }
+
+        function getPhone() {
+            return $this->phone;
+        }
+
+        function getAddress() {
+            return $this->address;
+        }
 
         // setters
+        function setName($name) {
+            $this->name = $name;
+        }
 
-        //save the particular CD instance in the session array
+        function setName($phone) {
+            $this->phone = $phone;
+        }
+
+        function setName($address) {
+            $this->address = $address;
+        }
+
+        // save the particular contact in the session array
         function save() {
             array_push($_SESSION['contacts'], $this);
         }
-        static function getCDs() {
+
+        // get existing contancts
+        static function getAll() {
             return $_SESSION['contacts'];
+        }
+
+        // delete existing contacts
+        static function deleteAll() {
+            $_SESSION['contacts'] = [];
         }
     }
 ?>
