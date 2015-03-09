746b5612348fee8574df6a3e5c0ab8de285dce5e
Daniel Toader & James Nielson <student@epicodus.com>
2015-03-09 12:39:27 -0700

    add apple exceptions

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-The Title Case program capitalizes the first letter of all words in a multiple-word title. For example, we expect it to change the color purple to The Color Purple.
+The Title Case program capitalizes the first letter of "all" words in a multiple-word title. For example, we expect it to change the color purple to The Color Purple.  Exceptions exist that won't be capitalized (a, an, the, etc), and there are exceptions within that group as well (firs and last words).  Other exceptions include trendy camel-case names like iTunes, etc.
 */
 
     class TitleCaseGenerator
