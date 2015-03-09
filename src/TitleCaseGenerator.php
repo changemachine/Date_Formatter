43098ea8911d857e395c773228f2fc0c58855af6
Daniel Toader & James Nielson <student@epicodus.com>
2015-03-09 10:04:05 -0700

    add phpunit testing structure

---
+++ src/TitleCaseGenerator.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+The Title Case program capitalizes the first letter of all words in a multiple-word title. For example, we expect it to change the color purple to The Color Purple.
+*/
+
+    class TitleCaseGenerator
+    {
+        function makeTitleCase($input_title)
+        {
+
+        }
+    }
+
+
+ ?>
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit55280f2bd9a1f04c875ed1e99ef70331::getLoader();
+return ComposerAutoloaderInit122adebc53dbb8ccca86ed4cd3193981::getLoader();
